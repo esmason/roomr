b5eb234ce9770767a879360ca674505c0cdb9f12
esmason <esmason66@gmail.com>
2017-02-12 19:52:37 -0800

    more boilerplate

---
+++ imports/api/times.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Times = new Mongo.Collection('times');
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,2 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
 
+import { Tasks } from '../api/times.js';
