f2cd3514b9ba612ade2eb2f47e4919e3f164a839
JordanRoth <Jordan-AR@hotmail.com>
2017-02-20 21:44:14 -0800

    code cleanup

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-import ReactDOM from 'react-dom';
 import { Times } from '../api/times.js';
 import Time from './Time.jsx';
 import SearchBar from './SearchBar.jsx';
 import MyMap from './MyMap.jsx';
-import { Meteor } from 'meteor/meteor'
-
 
 // App component - represents the whole app
 class App extends Component {
     constructor(props){
-        Meteor.call('populateBuildingDatabase', 1, 100, true, ["ANTH"]);
         super(props);
         this.state = {
             userLocation: null
@@ -24,7 +20,6 @@ class App extends Component {
             userLocation: event.latLng
         });
     }
-
 
     renderTimes() {
         return this.props.times.map((time) => (
@@ -55,12 +50,7 @@ class App extends Component {
             </div>
         );
     }
-
-
-
-
 }
-
 
 App.propTypes = {
     times: PropTypes.array.isRequired,
--- imports/ui/SearchBar.jsx
+++ imports/ui/SearchBar.jsx
@@ -7,37 +7,36 @@ import MyMap from './MyMap.jsx';
 
 
 export default class SearchBar extends Component{
-render(){
-  return(
-    <div>
-  <form className="search-db" onSubmit={this.handleSubmit.bind(this)} >
-    <input
-      type="text"
-      ref="textInput"
-      placeholder={this.props.placeholder}
-    />
-  </form>
-</div>
-)
-}
+    render(){
+        return(
+            <div>
+                <form className="search-db" onSubmit={this.handleSubmit.bind(this)} >
+                    <input
+                        type="text"
+                        ref="textInput"
+                        placeholder={this.props.placeholder}
+                    />
+                </form>
+            </div>
+        )
+    }
 
-handleSubmit(event) {
-  event.preventDefault();
+    handleSubmit(event) {
+        event.preventDefault();
 
-  // Find the text field via the React ref
-  const text1 = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        // Find the text field via the React ref
+        const text1 = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-  Times.find(
-      {}
-    ).fetch().map((time) => (Times.update({_id: time._id}, {$set :{show: false}}) ));
+        Times.find(
+            {}
+        ).fetch().map((time) => (Times.update({_id: time._id}, {$set :{show: false}}) ));
 
-Times.find(
-    {text :{$regex: '.*'.concat(text1, ".*")} }
-  ).fetch().map((time) => (Times.update({_id: time._id}, {$set :{show: true}}) ));
+        Times.find(
+            {text :{$regex: '.*'.concat(text1, ".*")} }
+        ).fetch().map((time) => (Times.update({_id: time._id}, {$set :{show: true}}) ));
 
-  // Clear form
-  ReactDOM.findDOMNode(this.refs.textInput).value = '';
-}
-
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
 
 }
