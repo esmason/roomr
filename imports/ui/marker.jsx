ed48568258c154c4703feeaebe30108c45283d6e
esmason <esmason66@gmail.com>
2017-03-06 14:33:16 -0800

    refactor markers to component to simplify re render logic

--- imports/ui/marker.jsx
+++ imports/ui/marker.jsx
@@ -1,1 +1,17 @@
 import React, { Component, PropTypes } from 'react';
+
+
+
+export default class Marker extends React.Component {
+
+    componentDidMount() {
+        this.props.marker.setMap(this.props.map);
+    }
+
+    componentWillUnmount(){
+        this.props.marker.setMap(null);
+    }
+
+    render(){return null;}
+
+}
